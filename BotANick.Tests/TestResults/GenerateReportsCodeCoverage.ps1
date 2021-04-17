bdf26d0dcfdf4fe8f8889e8e15997b0e5b29ce9e
Kitarsh <thomas.leclere@centrale-marseille.fr>
2021-04-17 17:49:29 +0200

    Adding Code coverage to SonarCloud analysis

---
+++ BotANick.Tests/TestResults/GenerateReportsCodeCoverage.ps1
@@ -0,0 +1,6 @@
+﻿Get-ChildItem -Recurse -Filter "*.coverage" | % {
+    $outfile = "$([System.IO.Path]::GetFileNameWithoutExtension($_.FullName)).coveragexml"
+    $output = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($_.FullName), $outfile)
+    "Analyse '$($_.Name)' with output '$outfile'..."
+    . ".\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:$output $_.FullName
+}
